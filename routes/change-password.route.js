2fb83899a6ce63fa1873731e3907be7e63d0e6b7
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-05-21 21:35:13 +0300

    feat: change password functionality

--- routes/change-password.route.js
+++ routes/change-password.route.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
   getChangePasswordPage,
+  updatePassword,
 } = require("../controllers/change-password.controller");
 
 router.get("/", getChangePasswordPage);
+router.put("/", updatePassword);
 
 module.exports = router;
