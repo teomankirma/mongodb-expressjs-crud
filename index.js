62eb0aca20ca135580d648e2568eb550787df690
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-05-21 13:03:46 +0300

    feat: create index.html file

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const mongoose = require("mongoose");
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  // Send the "index.html" file
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 mongoose
