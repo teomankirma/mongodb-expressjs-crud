d23ad359016f615547312ac9c266f73983ec70dc
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-05-21 14:17:46 +0300

    feat: signup logic

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const mongoose = require("mongoose");
 const signupRoute = require("./routes/signup.route");
 const signinRoute = require("./routes/signin.route");
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
--- routes/signup.route.js
+++ routes/signup.route.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getSignupPage } = require("../controllers/signup.controller");
+const {
+  getSignupPage,
+  postSignup,
+} = require("../controllers/signup.controller");
 
 router.get("/", getSignupPage);
+router.post("/", postSignup);
 
 module.exports = router;
