d4f5a6e39819de85da56de1e626f68bf390b6c7a
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-05-29 02:28:15 +0300

    feat: render description and single ticket price

--- models/ticket-type.model.js
+++ models/ticket-type.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const ticketTypeSchema = new mongoose.Schema({
   type: String,
   basePrice: Number,
+  description: String,
 });
 
 const TicketType = mongoose.model("Ticket_Type", ticketTypeSchema);
